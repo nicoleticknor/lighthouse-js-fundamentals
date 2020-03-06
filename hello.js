faa1952549a9f3bd8b225af8b85eb3e2cbd1a0a4
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-03-06 18:49:19 +0000

    Actually committing the changes to hello to add ;s

--- hello.js
+++ hello.js
@@ -1,9 +1,9 @@
 const sayHello = function(number) {
-  let count = 1
+  let count = 1;
   for(let i = 0; i < number; i++) {
   console.log("Hello, world " + count);
-  count += 1
-  }return count
+  count += 1;
+  }return count;
 };
 
 sayHello(2);
@@ -18,9 +18,25 @@ const sayHello2 = function(number) {
 sayHello2(1);
 
 const sayHello3 = function (name) {
-  console.log(`Hello, ${name}!`)
+  console.log(`Hello, ${name}!`);
 }
 
 // sayHello3("Caliban");
 // sayHello3("Miranda");
 sayHello3("Margo");
+
+
+/* ============================
+using return in a function */
+
+const sayHelloToConsole = function (name) {
+  console.log(`Hello, ${name}!`);
+};
+
+sayHelloToConsole("Crosby");
+
+const returnSayHello = function (name) {
+  return "Hello, " + name
+};
+const greeting = returnSayHello("Crosby");
+console.log(greeting);
