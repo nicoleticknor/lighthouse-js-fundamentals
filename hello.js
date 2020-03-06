f1c7fcecd665e594afcb64a90157f79aab04679f
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-03-06 18:04:31 +0000

    Say Hello with Functions

---
+++ hello.js
@@ -0,0 +1,26 @@
+const sayHello = function(number) {
+  let count = 1
+  for(let i = 0; i < number; i++) {
+  console.log("Hello, world " + count);
+  count += 1
+  }return count
+};
+
+sayHello(2);
+
+const sayHello2 = function(number) {
+  let people = ['Crosby', 'margo', 'nicole', 'luigi']
+  for(let i = 0; i < number; i++) {
+  console.log("Hello, " + people[i]+"!");
+  } 
+};
+
+sayHello2(1);
+
+const sayHello3 = function (name) {
+  console.log(`Hello, ${name}!`)
+}
+
+// sayHello3("Caliban");
+// sayHello3("Miranda");
+sayHello3("Margo");
